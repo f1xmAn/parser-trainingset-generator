183ff77df40f81122c791e401ab59fbd7f342191
Timur Yarosh <tyarosh@voiptime.net>
2021-08-06 21:12:30 +0300

    Initial commit

---
+++ src/main/java/com/github/f1xman/parsgen/ParserTrainingsetGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.github.f1xman.parsgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParserTrainingsetGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParserTrainingsetGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/f1xman/parsgen/ParserTrainingsetGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.f1xman.parsgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParserTrainingsetGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
