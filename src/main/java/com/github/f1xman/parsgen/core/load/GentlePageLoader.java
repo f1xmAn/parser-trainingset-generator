40b15966b8d72202e7ad10303d014af868cdd7f4
Timur Yarosh <tyarosh@voiptime.net>
2021-08-08 17:21:24 +0300

    load html pages from different sources in parallel and from the same source sequentially after the configurable pause

---
+++ src/main/java/com/github/f1xman/parsgen/core/load/GentlePageLoader.java
@@ -0,0 +1,79 @@
+package com.github.f1xman.parsgen.core.load;
+
+import com.github.f1xman.parsgen.core.load.model.LoadedPage;
+import com.github.f1xman.parsgen.core.load.model.LoadedPageImpl;
+import com.github.f1xman.parsgen.core.load.model.SourceCollection;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.Comparator.naturalOrder;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Loads html pages from different sources in parallel and from the same source sequentially.
+ * A pause can be configured for sequential calls. It's 10 seconds by default.
+ */
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class GentlePageLoader implements PageLoader {
+
+    private final SourceCollectionRepository sourceCollectionRepository;
+    private final HtmlPageLoader loader;
+    @Setter
+    private Duration pauseBetweenSlices = Duration.ofSeconds(10);
+
+    @Override
+    public List<LoadedPage> loadAll(int page, int size) {
+        return getUrlStreamSlices(page, size)
+                .flatMap(this::loadSlice)
+                .collect(toList());
+    }
+
+    /**
+     * Loads url streams after a pause. Pause introduced to slower calls between slices.
+     *
+     * @param urlStream stream of {@link URL} to download
+     * @return Stream of successfully loaded {@link LoadedPage}
+     */
+    @SneakyThrows
+    private Stream<LoadedPage> loadSlice(Stream<URL> urlStream) {
+        TimeUnit.SECONDS.sleep(pauseBetweenSlices.getSeconds());
+        return urlStream
+                .map(this::load)
+                .map(CompletableFuture::join);
+    }
+
+    private CompletableFuture<LoadedPage> load(URL url) {
+        return loader.load(url).thenApply(LoadedPageImpl::from);
+    }
+
+    private Stream<Stream<URL>> getUrlStreamSlices(int page, int size) {
+        List<SourceCollection> sourceCollections = sourceCollectionRepository.findAll(page, size);
+        Integer numberOfSlices = sourceCollections.stream()
+                .map(s -> s.getUrls().size())
+                .max(naturalOrder())
+                .orElse(0);
+        return IntStream.range(0, numberOfSlices)
+                .mapToObj(i -> getSlice(sourceCollections, i));
+    }
+
+    private Stream<URL> getSlice(List<SourceCollection> sourceCollections, int index) {
+        return sourceCollections.stream()
+                .map(SourceCollection::getUrls)
+                .filter(urls -> urls.size() > index)
+                .map(urls -> urls.get(index));
+    }
+
+}
--- src/test/java/com/github/f1xman/parsgen/core/load/PageLoaderImplTest.java
+++ src/test/java/com/github/f1xman/parsgen/core/load/PageLoaderImplTest.java
@@ -7,23 +7,31 @@ import com.github.f1xman.parsgen.core.load.model.SourceCollection;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.net.URL;
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.inOrder;
 
 @ExtendWith(MockitoExtension.class)
 class PageLoaderImplTest {
 
     static final int PAGE = 1;
     static final int SIZE = 1;
-    static final String BASE_URL = "baseUrl";
-    static final URL HTTPS_FOO_BAR_BAZ = url();
+    static final URL SOURCE_1_URL_1 = url("https://foo.bar/baz1");
+    static final URL SOURCE_1_URL_2 = url("https://foo.bar/baz2");
+    static final URL SOURCE_2_URL_1 = url("https://foo.baz/bar1");
+    static final List<SourceCollection> SOURCE_COLLECTIONS = List.of(
+            SourceCollection.of("https://boo.bar", List.of(SOURCE_1_URL_1, SOURCE_1_URL_2)),
+            SourceCollection.of("https://boo.baz", List.of(SOURCE_2_URL_1))
+    );
     static final String CONTENT = "content";
 
     @Mock
@@ -31,22 +39,33 @@ class PageLoaderImplTest {
     @Mock
     HtmlPageLoader htmlPageLoader;
 
+    @SneakyThrows
+    private static URL url(String url) {
+        return new URL(url);
+    }
+
     @Test
     void queriesSourceCollectionsAndLoadsPages() {
-        PageLoaderImpl pageLoader = new PageLoaderImpl(sourceCollectionRepository, htmlPageLoader);
-        List<SourceCollection> sourceCollections = List.of(SourceCollection.of(BASE_URL, List.of(HTTPS_FOO_BAR_BAZ)));
-        given(sourceCollectionRepository.findAll(PAGE, SIZE)).willReturn(sourceCollections);
-        HtmlPage htmlPage = HtmlPage.of(HTTPS_FOO_BAR_BAZ, CONTENT);
-        given(htmlPageLoader.load(HTTPS_FOO_BAR_BAZ)).willReturn(CompletableFuture.completedFuture(htmlPage));
-        List<LoadedPage> expectedLoadedPages = List.of(LoadedPageImpl.from(htmlPage));
+        GentlePageLoader pageLoader = new GentlePageLoader(sourceCollectionRepository, htmlPageLoader);
+        pageLoader.setPauseBetweenSlices(Duration.ZERO);
+        given(sourceCollectionRepository.findAll(PAGE, SIZE)).willReturn(SOURCE_COLLECTIONS);
+        HtmlPage source1Page1 = HtmlPage.of(SOURCE_1_URL_1, CONTENT);
+        given(htmlPageLoader.load(SOURCE_1_URL_1)).willReturn(CompletableFuture.completedFuture(source1Page1));
+        HtmlPage source1Page2 = HtmlPage.of(SOURCE_1_URL_2, CONTENT);
+        given(htmlPageLoader.load(SOURCE_1_URL_2)).willReturn(CompletableFuture.completedFuture(source1Page2));
+        HtmlPage source2Page1 = HtmlPage.of(SOURCE_2_URL_1, CONTENT);
+        given(htmlPageLoader.load(SOURCE_2_URL_1)).willReturn(CompletableFuture.completedFuture(source2Page1));
 
         List<LoadedPage> actualLoadedPages = pageLoader.loadAll(PAGE, SIZE);
 
-        then(actualLoadedPages).isEqualTo(expectedLoadedPages);
-    }
-
-    @SneakyThrows
-    private static URL url() {
-        return new URL("https://foo.bar/baz");
+        then(actualLoadedPages).contains(
+                LoadedPageImpl.from(source1Page1),
+                LoadedPageImpl.from(source2Page1),
+                LoadedPageImpl.from(source1Page2)
+        );
+        InOrder inOrder = inOrder(htmlPageLoader);
+        inOrder.verify(htmlPageLoader).load(SOURCE_1_URL_1);
+        inOrder.verify(htmlPageLoader).load(SOURCE_2_URL_1);
+        inOrder.verify(htmlPageLoader).load(SOURCE_1_URL_2);
     }
 }
