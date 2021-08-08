4cf3b64a6c4fa5f0b7743609bf746ab8758f6470
Timur Yarosh <tyarosh@voiptime.net>
2021-08-08 17:23:31 +0300

    run contextLoads test against mongodb from the testcontainers

--- src/test/java/com/github/f1xman/parsgen/ParserTrainingsetGeneratorApplicationTests.java
+++ src/test/java/com/github/f1xman/parsgen/ParserTrainingsetGeneratorApplicationTests.java
@@ -2,9 +2,24 @@ package com.github.f1xman.parsgen;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class ParserTrainingsetGeneratorApplicationTests {
+
+    @Container
+    static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:focal"));
+
+    @DynamicPropertySource
+    static void mongoDbProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Test
     void contextLoads() {
